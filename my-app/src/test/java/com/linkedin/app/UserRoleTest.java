2064f8bdbbbb972bc05577105fe198d11d8a1b60
dilesanmi <dilesanmi@copyright.com>
2025-07-25 10:50:22 +0000

    param etst

--- my-app/src/test/java/com/linkedin/app/UserRoleTest.java
+++ my-app/src/test/java/com/linkedin/app/UserRoleTest.java
@@ -1,10 +1,28 @@
 package com.linkedin.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class UserRoleTest {
+
+
+  @ParameterizedTest
+  @EnumSource (value = UserRole.class, names= {"ADMIN", "MODERATOR"})
+  void rolesWithWritePermission(UserRole role){
+    assertTrue(role.hasWritePermission());
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = UserRole.class, names = { "USER", "GUEST" })
+  void rolesWithoutWritePermission(UserRole role) {
+    assertFalse(role.hasWritePermission());
+  }
+
+
 
   @Test
   void adminHasWritePermission() {
